d6cf49fae76600a2f60ca4f852e83cb04100c87b
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2016-12-04 13:36:16 -0500

    add buyer and seller routes

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var https = require('https');
 
 var express = require('express');
 var app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+//LOAD HTTPS SERVER
 var options = {
-  key: fs.readFileSync('security/file.pem'),
-  cert: fs.readFileSync('security/file.cert')
+    key: fs.readFileSync('security/file.pem'),
+    cert: fs.readFileSync('security/file.cert')
 };
 
 var serverPort = 3000;
@@ -17,15 +18,26 @@ var serverPort = 3000;
 var server = https.createServer(options, app);
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/views/index.html')
+// ROUTES
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/views/index.html')
 });
 
-io.on('connection', function(socket){
-  console.log('New socket connection from secure server!');
-  socket.emit('message', 'This message is comming from a secure server!')
+app.get('/buyer-chat', function(req, res){
+  res.send('buyer chat');
 });
 
-server.listen(serverPort, function(){
-  console.log("Secure server up at port " + serverPort);
+app.get('/seller-home', function(req, res){
+  res.send('seller home');
+});
+
+// SOCKET
+io.on('connection', function(socket) {
+    console.log('New socket connection from secure server!');
+
+});
+
+// SERVER LISTENING
+server.listen(serverPort, function() {
+    console.log("Secure server up at port " + serverPort);
 });
