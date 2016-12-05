e5804c02c685062a7bbd754ad53def8191f4a3cb
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2016-12-04 20:31:35 -0500

    create logic for server to accept information from client

--- app.js
+++ app.js
@@ -38,13 +38,29 @@ io.on('connection', function(socket) {
         console.log('user disconnected');
     });
 
-    socket.on('buyer-chat-active', function(){
-      socket.emit('welcome-message');
+    socket.on('buyer-chat-active', function() {
+        socket.emit('welcome-message');
     });
 
-    socket.on('chat message', function(msg){
-       console.log('message: ' + msg);
-     });
+    socket.on('nav', function(data) {
+        switch (data.ui) {
+            case "learn":
+                socket.emit('message', { server: 'This app helps you buy right now! Tap the button below to take a photo of the name card by the product. We will take care of the rest.' });
+                break;
+            case "camera":
+                console.log("inside camera block");
+                break;
+            case "account":
+                socket.emit('message', {server: 'We can handle that later, go ahead and take a picture!'});
+                break;
+            default:
+
+        }
+    });
+
+    // socket.on('chat message', function(msg){
+    //    console.log('message: ' + msg);
+    //  });
 });
 
 // SERVER LISTENING
