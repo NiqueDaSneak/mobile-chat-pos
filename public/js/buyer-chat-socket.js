201556dcca20267524ec3e0dd4f946a34af088b3
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2016-12-04 20:30:38 -0500

    build logic for ui to send messages to server

--- public/js/buyer-chat-socket.js
+++ public/js/buyer-chat-socket.js
@@ -6,13 +6,31 @@ $(document).ready(function(){
   socket.emit('buyer-chat-active');
 
   socket.on('welcome-message', function(){
-    $('#messages').append("<li id='messages'>Testing</li>");
-    console.log('testing');
+    $('#messages').append("<li id='messages'>Welcome to the chat. Choose an option...</li>");
   });
 
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+  socket.on('message', function(data){
+    $('#messages').append("<li id='messages'>" + data.server + "</li>");
   });
+
+  $('.ui-1').click(function(){
+    socket.emit('nav', { ui: "learn" });
+  });
+
+  $('.ui-2').click(function(){
+    $('input').click();
+    socket.emit('nav', { ui: "camera" });
+  });
+
+  $('.ui-3').click(function(){
+    socket.emit('nav', { ui: "account" });
+  });
+
+
+
+  // $('form').submit(function(){
+  //   socket.emit('chat message', $('#m').val());
+  //   $('#m').val('');
+  //   return false;
+  // });
 });
